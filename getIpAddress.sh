f30859776ed9142aeb4de1d3ddf9112cab60797e
Korey Earl <korey.earl@gmail.com>
2019-11-20 23:00:43 +0000

    creates brick database

--- getIpAddress.sh
+++ getIpAddress.sh
@@ -1,2 +1,4 @@
-jq '.Reservations[] | .Instances[]| .PublicIpAddress' instances.json
+#! /bin/bash
+
+aws ec2 describe-instances | jq -r '.Reservations[] | .Instances[]| select(has("Tags") and .State.Name == "running") | .PublicIpAddress'
 
